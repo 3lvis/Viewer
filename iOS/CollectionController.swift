5cf60720a5cd6cc0001e3403582183dbbaaf3f94
Elvis Nuñez <elvisnunez@me.com>
2015-12-08 16:17:02 +0100

    Add scaling down

--- iOS/CollectionController.swift
+++ iOS/CollectionController.swift
@@ -8,6 +8,19 @@ class CollectionController: UICollectionViewController {
 
         self.collectionView?.registerClass(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.Identifier)
     }
+
+    lazy var overlayView: UIView = {
+        let view = UIView(frame: UIScreen.mainScreen().bounds)
+        view.backgroundColor = UIColor.blackColor()
+        view.alpha = 0
+        view.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+
+        return view
+    }()
+
+    var cell: UIImageView?
+    var originalRect = CGRectZero
+    var selectedIndexPath = NSIndexPath(forRow: 0, inSection: 0)
 }
 
 extension CollectionController {
@@ -26,18 +39,17 @@ extension CollectionController {
     }
 
     override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        self.selectedIndexPath = indexPath
+
         if let existingCell = collectionView.cellForItemAtIndexPath(indexPath) {
             existingCell.alpha = 0
 
             guard let window = UIApplication.sharedApplication().delegate?.window?! else { return }
 
-            let overlayView = UIView(frame: UIScreen.mainScreen().bounds)
-            overlayView.backgroundColor = UIColor.blackColor()
-            overlayView.alpha = 0
-            overlayView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
             window.addSubview(overlayView)
 
             let convertedRect = window.convertRect(existingCell.frame, fromView: self.collectionView!)
+            self.originalRect = convertedRect
             let transformedCell = UIImageView(frame: convertedRect)
             transformedCell.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
             transformedCell.contentMode = .ScaleAspectFill
@@ -52,7 +64,7 @@ extension CollectionController {
                 let finalImageViewFrame = CGRectMake(0, (screenBound.size.height/2) - ((transformedCell.image!.size.height / scaleFactor)/2), screenBound.size.width, transformedCell.image!.size.height / scaleFactor)
 
                 UIView.animateWithDuration(0.25, animations: {
-                    overlayView.alpha = 1.0
+                    self.overlayView.alpha = 1.0
                     transformedCell.frame = finalImageViewFrame
                     }, completion: { finished in
                         let viewerController = ViewerController(pageIndex: indexPath.row)
@@ -60,7 +72,8 @@ extension CollectionController {
                         viewerController.controllerDataSource = self
                         self.presentViewController(viewerController, animated: false, completion: {
                             transformedCell.removeFromSuperview()
-                            overlayView.removeFromSuperview()
+                            self.cell = transformedCell
+                            self.overlayView.removeFromSuperview()
                         })
                 })
             }
@@ -79,6 +92,28 @@ extension CollectionController: ViewerControllerDelegate {
     }
 
     func viewerControllerDidDismiss(viewerController: ViewerController) {
-        self.dismissViewControllerAnimated(true, completion: nil)
+        self.dismissViewControllerAnimated(false, completion: nil)
+
+        let screenBound = UIScreen.mainScreen().bounds
+        let transformedCell = self.cell!
+        let scaleFactor = transformedCell.image!.size.width / screenBound.size.width
+        transformedCell.frame = CGRectMake(0, (screenBound.size.height/2) - ((transformedCell.image!.size.height / scaleFactor)/2), screenBound.size.width, transformedCell.image!.size.height / scaleFactor)
+
+        self.overlayView.alpha = 1.0
+        guard let window = UIApplication.sharedApplication().delegate?.window?! else { return }
+        window.addSubview(overlayView)
+        window.addSubview(transformedCell)
+
+        UIView.animateWithDuration(0.25, animations: {
+            self.overlayView.alpha = 0.0
+            transformedCell.frame = self.originalRect
+            }, completion: { finished in
+                if let existingCell = self.collectionView?.cellForItemAtIndexPath(self.selectedIndexPath) {
+                    existingCell.alpha = 1
+                }
+
+                transformedCell.removeFromSuperview()
+                self.overlayView.removeFromSuperview()
+        })
     }
 }
