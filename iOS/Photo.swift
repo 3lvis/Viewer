2b800cd846257ae818326f6db98ad645be17ddcd
Elvis Nuñez <elvisnunez@me.com>
2015-12-09 17:48:10 +0100

    Add super wide image + refactor images

--- iOS/Photo.swift
+++ iOS/Photo.swift
@@ -14,19 +14,22 @@ struct Photo: ViewerItem {
         for i in 1..<60 {
             var photo = Photo(id: i)
 
-            let index = Int(arc4random_uniform(4))
+            let index = Int(arc4random_uniform(5))
             switch index {
             case 0:
-                photo.image = UIImage(named: "a.jpg")
+                photo.image = UIImage(named: "0.jpg")
                 break
             case 1:
-                photo.image = UIImage(named: "b.jpg")
+                photo.image = UIImage(named: "1.jpg")
                 break
             case 2:
-                photo.image = UIImage(named: "c.jpg")
+                photo.image = UIImage(named: "2.jpg")
                 break
             case 3:
-                photo.image = UIImage(named: "d.jpg")
+                photo.image = UIImage(named: "3.jpg")
+                break
+            case 4:
+                photo.image = UIImage(named: "4.jpg")
                 break
             default: break
             }
