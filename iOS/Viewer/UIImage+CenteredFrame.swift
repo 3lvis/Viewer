6d581340e831e99255c50be02a4afe87c622ce1e
Elvis Nuñez <elvisnunez@me.com>
2015-12-10 10:32:14 +0100

    Improve naming

--- iOS/Viewer/UIImage+CenteredFrame.swift
+++ iOS/Viewer/UIImage+CenteredFrame.swift
@@ -5,17 +5,17 @@ extension UIImage {
         let screenBounds = UIScreen.mainScreen().bounds
         let widthScaleFactor = self.size.width / screenBounds.size.width
         let heightScaleFactor = self.size.height / screenBounds.size.height
-        var finalImageViewFrame = CGRectZero
+        var centeredFrame: CGRect
 
         let shouldFitHorizontally = widthScaleFactor > heightScaleFactor
         if shouldFitHorizontally {
             let y = (screenBounds.size.height / 2) - ((self.size.height / widthScaleFactor) / 2)
-            finalImageViewFrame = CGRectMake(0, y, screenBounds.size.width, self.size.height / widthScaleFactor)
+            centeredFrame = CGRectMake(0, y, screenBounds.size.width, self.size.height / widthScaleFactor)
         } else {
             let x = (screenBounds.size.width / 2) - ((self.size.width / heightScaleFactor) / 2)
-            finalImageViewFrame = CGRectMake(x, 0, screenBounds.size.width - (2 * x), screenBounds.size.height)
+            centeredFrame = CGRectMake(x, 0, screenBounds.size.width - (2 * x), screenBounds.size.height)
         }
 
-        return finalImageViewFrame
+        return centeredFrame
     }
 }
