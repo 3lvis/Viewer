c0460998b3c5c841ad1d4f57c755e9bdc881e669
Elvis Nuñez <elvisnunez@me.com>
2015-12-15 12:41:37 +0100

    Add buttons to HeaderView

--- iOS/HeaderView.swift
+++ iOS/HeaderView.swift
@@ -1,10 +1,32 @@
 import UIKit
 
 class HeaderView: UIView {
+    static let ButtonSize = CGFloat(50.0)
+
+    lazy var clearButton: UIButton = {
+        let image = UIImage(named: "clear")!
+        let button = UIButton(type: .Custom)
+        button.frame = CGRect(x: 0, y: 0, width: HeaderView.ButtonSize, height: HeaderView.ButtonSize)
+        button.setImage(image, forState: .Normal)
+
+        return button
+    }()
+
+    lazy var menuButton: UIButton = {
+        let image = UIImage(named: "menu")!
+        let button = UIButton(type: .Custom)
+        let x = UIScreen.mainScreen().bounds.size.width - HeaderView.ButtonSize
+        button.frame = CGRect(x: x, y: 0, width: HeaderView.ButtonSize, height: HeaderView.ButtonSize)
+        button.setImage(image, forState: .Normal)
+
+        return button
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        self.backgroundColor = UIColor.redColor()
+        self.addSubview(self.clearButton)
+        self.addSubview(self.menuButton)
     }
 
     required init?(coder aDecoder: NSCoder) {
