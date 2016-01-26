8951cb52f173f380d0cc40d764c4ea1a5168d7a2
Elvis Nuñez <elvisnunez@me.com>
2016-01-26 17:04:09 +0100

    Add sample video loading

--- Source/ViewerItem.swift
+++ Source/ViewerItem.swift
@@ -3,6 +3,7 @@ import UIKit
 public protocol ViewerItem {
     var remoteID: String? { get }
     var placeholder: UIImage { get set }
+    var url: String? { get set }
 
     func media(completion: (image: UIImage?) -> ())
 }
--- Source/ViewerItemController.swift
+++ Source/ViewerItemController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import AVFoundation
+import AVKit
 
 protocol ViewerItemControllerDelegate: class {
     func viewerItemControllerDidTapItem(viewerItemController: ViewerItemController, completion: (() -> Void)?)
@@ -11,11 +13,20 @@ class ViewerItemController: UIViewController {
         didSet {
             if let viewerItem = self.viewerItem {
                 self.imageView.image = viewerItem.placeholder
-                viewerItem.media({ image in
-                    if let image = image {
-                        self.imageView.image = image
-                    }
-                })
+
+                if let url = viewerItem.url {
+                    self.loadingIndicator.startAnimating()
+                    let steamingURL = NSURL(string: url)!
+                    let player = AVPlayer(URL: steamingURL)
+                    player.addObserver(self, forKeyPath: "status", options: [], context: nil)
+                    self.playerLayer.player = player
+                } else {
+                    viewerItem.media({ image in
+                        if let image = image {
+                            self.imageView.image = image
+                        }
+                    })
+                }
             }
         }
     }
@@ -32,6 +43,21 @@ class ViewerItemController: UIViewController {
         return view
     }()
 
+    lazy var playerLayer: AVPlayerLayer = {
+        let playerLayer = AVPlayerLayer()
+        playerLayer.frame = self.view.frame
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.view.layer.addSublayer(playerLayer)
+
+        return playerLayer
+    }()
+
+    lazy var loadingIndicator: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView(activityIndicatorStyle: .White)
+
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,11 +65,25 @@ class ViewerItemController: UIViewController {
         self.view.addSubview(self.imageView)
         self.view.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
 
+        self.loadingIndicator.center = self.view.center
+        self.view.addSubview(self.loadingIndicator)
+
         let tapRecognizer = UITapGestureRecognizer(target: self, action: "tapAction")
         self.view.addGestureRecognizer(tapRecognizer)
     }
 
     func tapAction() {
         self.controllerDelegate?.viewerItemControllerDidTapItem(self, completion: nil)
+    }
+
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        guard let player = object as? AVPlayer else { return }
+
+        if player.status == .ReadyToPlay {
+            self.loadingIndicator.stopAnimating()
+            player.play()
+        } else {
+            print("BURNING BURNING")
+        }
     }
 }
