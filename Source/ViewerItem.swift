504f64798c096142b93c14a48317cff5353ab821
Elvis Nuñez <3lvis@users.noreply.github.com>
2016-02-02 10:29:59 +0100

    Allow placeholder to be mutated

--- Source/ViewerItem.swift
+++ Source/ViewerItem.swift
@@ -2,7 +2,7 @@ import UIKit
 
 public protocol ViewerItem {
     var remoteID: String? { get }
-    var placeholder: UIImage { get }
+    var placeholder: UIImage { get set }
     var url: String? { get }
 
     func media(completion: (image: UIImage?) -> ())
