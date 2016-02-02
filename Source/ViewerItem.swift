f56ef1e18a239495dc1c21df54a347588faee69b
Elvis Nuñez <elvisnunez@me.com>
2016-02-02 10:27:00 +0100

    Remove set requirement

--- Source/ViewerItem.swift
+++ Source/ViewerItem.swift
@@ -2,8 +2,8 @@ import UIKit
 
 public protocol ViewerItem {
     var remoteID: String? { get }
-    var placeholder: UIImage { get set }
-    var url: String? { get set }
+    var placeholder: UIImage { get }
+    var url: String? { get }
 
     func media(completion: (image: UIImage?) -> ())
 }
