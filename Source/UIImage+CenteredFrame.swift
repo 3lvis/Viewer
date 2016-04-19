953f08edd5ce10e5ca0d53e92dfd5eef90430cc3
Elvis Nuñez <3lvis@users.noreply.github.com>
2016-04-19 21:36:43 +0200

    Avoid division by 0

--- Source/UIImage+CenteredFrame.swift
+++ Source/UIImage+CenteredFrame.swift
@@ -5,13 +5,13 @@ extension UIImage {
         let screenBounds = UIScreen.mainScreen().bounds
         let widthScaleFactor = self.size.width / screenBounds.size.width
         let heightScaleFactor = self.size.height / screenBounds.size.height
-        var centeredFrame: CGRect
+        var centeredFrame = CGRectZero
 
         let shouldFitHorizontally = widthScaleFactor > heightScaleFactor
-        if shouldFitHorizontally {
+        if shouldFitHorizontally && widthScaleFactor > 0 {
             let y = (screenBounds.size.height / 2) - ((self.size.height / widthScaleFactor) / 2)
             centeredFrame = CGRectMake(0, y, screenBounds.size.width, self.size.height / widthScaleFactor)
-        } else {
+        } else if heightScaleFactor > 0 {
             let x = (screenBounds.size.width / 2) - ((self.size.width / heightScaleFactor) / 2)
             centeredFrame = CGRectMake(x, 0, screenBounds.size.width - (2 * x), screenBounds.size.height)
         }
