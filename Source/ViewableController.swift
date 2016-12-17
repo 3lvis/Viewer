2eff5954c45e96f5d372af4fe47c56357f762b61
Elvis Nuñez <elvisnunez@me.com>
2016-12-17 18:02:46 +0100

    Revert "Change buttons state even in failure"
    This reverts commit 3f2e17e4788decbec5b4d61e660097803d4a45ed.

--- Source/ViewableController.swift
+++ Source/ViewableController.swift
@@ -362,12 +362,12 @@ extension ViewableController: VideoViewDelegate {
     func videoViewDidFinishPlaying(_ videoView: VideoView, error: NSError?) {
         if let error = error {
             self.delegate?.viewableController(self, didFailPlayingVideoWith: error)
+        } else {
+            self.repeatButton.alpha = 1
+            self.pauseButton.alpha = 0
+            self.playButton.alpha = 0
+            self.videoProgressView.alpha = 0
         }
-
-        self.repeatButton.alpha = 1
-        self.pauseButton.alpha = 0
-        self.playButton.alpha = 0
-        self.videoProgressView.alpha = 0
     }
 }
 
